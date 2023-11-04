923b256f5613a64cd2312d4c2d61b972a4d4dee5
JinFree <ytrqwe12@gmail.com>
2023-11-04 15:18:04 +0900

    print report with openpyxl

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 import yaml
+import openpyxl
+from openpyxl.chart import PieChart, BarChart, Reference
 
 def ReadConfigs(yaml_path):
     data_dict = {}
@@ -10,15 +12,18 @@ def ReadConfigs(yaml_path):
 class ComputeTime():
     def __init__(self, data_dict):
         self.answer_list = []
+        self.ave_list = data_dict["Average"]
         self.question = data_dict["Question_list"]
         self.maximum_life = int(data_dict["Maximum_Life"])
         self.kid_maximun_age = int(data_dict["Kid_Maximum_Age"])
         self.remaining_time = 0
         self.gender = ""
         self.shortage_in_year = 0
-        self.shortage_in_hour = 0
+        self.shortage_in_hour = 0   
+        self.out_name = data_dict["Out_Name"] 
         
         pass
+    
     def __compute_year_to_hour__(self):
         self.shortage_in_hour = self.shortage_in_year * 365 * 24
         
@@ -80,7 +85,99 @@ class ComputeTime():
         self.answer_list.append(hour_with_kid)
         pass
     
+    def __time_with_kid__(self, hour_with_kid):
+        data = {
+            0.5: 15.7,
+            2: 43.6,
+            24: 40.6
+        }
+        for k, v in data.items():
+            if hour_with_kid < k:
+                return v
     def __generate_report__(self):
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+        sheet.append(["내 수면시간", "평균 수면시간"])
+        sheet.append([self.answer_list[0], self.ave_list[0]])
+        
+        sheet.append(["내 이동시간", "평균 이동시간"])
+        sheet.append([self.answer_list[1], self.ave_list[1]])
+        
+        sheet.append(["내 노동시간", "평균 노동시간"])
+        sheet.append([self.answer_list[2], self.ave_list[2]])
+        
+        sheet.append(["내 여가시간", "평균 여가시간"])
+        sheet.append([self.answer_list[3], self.ave_list[3]])
+        
+        left_hour = 24
+        left_hour -= self.answer_list[0]
+        left_hour -= self.answer_list[1] / 60.
+        left_hour -= self.answer_list[2]
+        left_hour -= self.answer_list[3]
+        data = [
+            ['항목', '시간'],
+            ['수면시간', self.answer_list[0]],
+            ['이동시간', self.answer_list[1] / 60.],
+            ['노동시간', self.answer_list[2]],
+            ['여가시간', self.answer_list[3]],
+            ['육아시간', self.answer_list[4]],
+            ['남은시간', left_hour]
+        ]
+        
+        for row in data:
+            sheet.append(row)
+            
+        values = Reference(sheet, min_col=1, min_row=1
+                           ,max_col=2, max_row=2)
+        chart = BarChart()
+        chart.add_data(values, titles_from_data=True)
+        chart.title = "평균 수면시간 대비 내 수면시간"
+        chart.y_axis.title = "수면시간"
+        chart.x_axis.title = "단위: 시간"
+        sheet.add_chart(chart, "A1")
+        
+        
+        values = Reference(sheet, min_col=1, min_row=3
+                           ,max_col=2, max_row=4)
+        chart = BarChart()
+        chart.add_data(values, titles_from_data=True)
+        chart.title = "평균 이동시간 대비 내 이동시간"
+        chart.y_axis.title = "이동시간"
+        chart.x_axis.title = "단위: 분"
+        sheet.add_chart(chart, "I1")
+        
+        
+        values = Reference(sheet, min_col=1, min_row=5
+                           ,max_col=2, max_row=6)
+        chart = BarChart()
+        chart.add_data(values, titles_from_data=True)
+        chart.title = "평균 노동시간 대비 내 노동시간"
+        chart.y_axis.title = "노동시간"
+        chart.x_axis.title = "단위: 시간"
+        sheet.add_chart(chart, "A14")
+        
+        
+        values = Reference(sheet, min_col=1, min_row=7
+                           ,max_col=2, max_row=8)
+        chart = BarChart()
+        chart.add_data(values, titles_from_data=True)
+        chart.title = "평균 여가시간 대비 내 여가시간"
+        chart.y_axis.title = "여가시간"
+        chart.x_axis.title = "단위: 시간"
+        sheet.add_chart(chart, "I14")
+        
+        pie = PieChart()
+        labels = Reference(sheet, min_col = 1, min_row = 10, max_row=15)
+        data = Reference(sheet, min_col = 2, min_row = 9, max_row=15)
+        pie.add_data(data, titles_from_data=True)
+        pie.set_categories(labels)
+        pie.title = "하루 시간표"
+        sheet.add_chart(pie, "A27")
+        
+        percents = self.__time_with_kid__(self.answer_list[4])
+        word = "당신의 자녀와 하루 놀이/대화시간은 전체 부모의 {}%에 속합니다.".format(percents)
+        sheet['J28'] = word
+        wb.save(self.out_name)
         pass 
         
         
@@ -96,7 +193,7 @@ class ComputeTime():
         print("당신이 자녀와 함께 할 수 있는 시간은 총 {} 시간 남았습니다.".format(int(self.shortage_in_hour)))
         print("당신이 자녀와 함께 할 수 있는 시간은 총 {} 일 남았습니다.".format(int(self.shortage_in_hour / 24)))
         print("당신이 자녀와 함께 할 수 있는 시간은 총 {} 년 남았습니다.".format(int(self.shortage_in_hour / (24 * 365))))
-
+        self.__generate_report__()
         pass
         
     
