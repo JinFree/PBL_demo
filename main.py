23b2a1a600061646e503683b5b2a483467be373b
JinFree <ytrqwe12@gmail.com>
2023-11-04 14:02:12 +0900

    기본설문작성

--- main.py
+++ main.py
@@ -9,11 +9,27 @@ def ReadConfigs(yaml_path):
 
 class ComputeTime():
     def __init__(self, data_dict):
-        answer_list = []
-        print(data_dict)
+        self.answer_list = []
         self.question = data_dict["Question_list"]
+        self.maximum_life = int(data_dict["Maximum_Life"])
         self.remaining_time = 0
+        self.gender = ""
+        self.shortage_in_year = 0
+        self.shortage_in_hour = 0
+        
         pass
+    def __compute_year_to_hour__(self):
+        self.shortage_in_hour = self.shortage_in_year * 365 * 24
+        
+    def __ask_gender__(self):
+        print("자녀와의 관계를 입력해주세요.")
+        answer = input("아버지 / 어머니: ")
+        if answer in ["아버지", "어머니"]:
+            self.gender = answer
+        else:
+            print("아버지 또는 어머니로 입력해주세요.")
+            return self.__ask_gender__()
+        
     def __question__(self, idx):
         if (idx >= len(self.question)):
             return 0
@@ -27,39 +43,51 @@ class ComputeTime():
             return self.__question__(idx)
         
     def __compute_q1__(self):
-        self.__question__(0)
-        self.__question__(1)
+        age_myself = self.__question__(0)
+        age_myself_shortage = self.maximum_life - age_myself
+        age_kid = self.__question__(1)
+        age_kid_shortage = 18 - age_kid
+        self.shortage_in_year = min(age_kid_shortage, age_myself_shortage)
+        self.__compute_year_to_hour__()
         pass
+    
     def __compute_q2__(self):
-        self.__question__(3)
+        hour_sleep = self.__question__(2)
+        self.answer_list.append(hour_sleep)
+        self.shortage_in_hour = self.shortage_in_hour - hour_sleep * 365
         pass
     def __compute_q3__(self):
-        self.__question__(4)
+        min_moving = self.__question__(3)
+        self.answer_list.append(min_moving)
+        self.shortage_in_hour = self.shortage_in_hour - min_moving * 1.0 / 60.0 * 365
         pass
     def __compute_q4__(self):
-        self.__question__(5)
+        hour_working = self.__question__(4)
+        self.answer_list.append(hour_working)
+        self.shortage_in_hour = self.shortage_in_hour - hour_working * 365
         pass
     def __compute_q5__(self):
-        self.__question__(6)
+        hour_relax = self.__question__(5)
+        self.answer_list.append(hour_relax)
+        self.shortage_in_hour = self.shortage_in_hour - hour_relax * 365
         pass
     def __compute_q6__(self):
-        self.__question__(7)
-        pass
-    def __compute_q7__(self):
-        self.__question__(8)
+        hour_with_kid = self.__question__(6)
+        self.answer_list.append(hour_with_kid)
+        
         pass
         
         
         
     def __call__(self):
+        self.__ask_gender__()
         self.__compute_q1__()
         self.__compute_q2__()
         self.__compute_q3__()
         self.__compute_q4__()
         self.__compute_q5__()
         self.__compute_q6__()
-        self.__compute_q7__()
-        
+        print("당신이 자녀와 함께 할 수 있는 시간은 총 {} 일 남았습니다.".format(int(self.shortage_in_hour / 24)))
         pass
         
     
